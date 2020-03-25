d8ad65f334afaf8793844e577c79be001abddd16
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-25 16:26:00 -0700

    Refactored code and added rotation to globe

--- src/index.js
+++ src/index.js
@@ -1,73 +1,76 @@
-import {
-	select,
-	json,
-	geoPath,
-	geoMercator,
-	geoOrthographic,
-	zoom,
-	event,
-	geoGraticule,
-	mouse,
-	queue
-} from "d3";
+import * as d3 from "d3";
 import { feature } from "topojson";
 import "../styles/styles.css";
 
-const svg = select("svg");
+const svg = d3.select("svg");
 const width = +svg.attr("width");
 const height = +svg.attr("height");
-const projection = geoOrthographic();
-const pathGenerator = geoPath().projection(projection);
+const projection = d3.geoOrthographic();
+const pathGenerator = d3.geoPath().projection(projection);
 
-const g = svg.append("g");
-
-// var graticule = geoGraticule()
-// .step([10, 10]);
-
-g.append("path")
-	.attr("class", "sphere")
-	.attr("d", pathGenerator({ type: "Sphere" }));
+// const g = svg.append("g");
 
 //TODO: consider adding debouncing, one scroll make ~100 events
-svg.call(
-	zoom().on("zoom", () => {
-		g.attr("transform", event.transform);
-	})
-);
+// svg.call(
+// 	d3.zoom().on("zoom", () => {
+// 		g.attr("transform", event.transform);
+// 	})
+// );
+drawEarth();
+drawGraticules();
+rotation();
+function drawEarth() {
+	svg.append("path")
+		.attr("class", "sphere")
+		.attr("d", pathGenerator({ type: "Sphere" }));
 
-json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(
-	data => {
+	d3.json(
+		"https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
+	).then(data => {
 		const countries = feature(data, data.objects.countries);
-		var graticule = geoGraticule().step([10, 10]);
-		// Add the graticule to the figure
-		g.append('path').datum(graticule)
-		.attr('class', 'graticule')
-		.attr('d', d => pathGenerator(d));
 
-		g.selectAll("path.feature")
+		svg.selectAll("path.feature")
 			.data(countries.features)
 			.enter()
 			.append("path")
 			.attr("class", "country")
 			.attr("d", d => pathGenerator(d))
 			.on("mouseover", function(d) {
-				// console.log(d)
-				select(this).style("fill", "lightgrey");
+				// d3.select(this).style("fill", "lightgrey");
+				d3.select(this).attr("class", "country-mouse-over");
 			})
 			.on("mouseout", function(d) {
-				select(this).style("fill", "white");
-			})
-			// .append("graticule")
-			// .append("path")
-			// .datum(graticule)
-			// .attr("class", "graticule")
-			// .attr("d", d => pathGenerator(d));
+				let c = d3.select(this);
+				c.attr("class", null);
+				c.attr("class", "country");
+			});
+	});
+}
 
-		// .append("title")
-		// .text(d => {
-		// 	return d.properties.name;
-		// });
-	}
-);
+function drawGraticules() {
+	// Add the graticule to the figure
+	const graticule = d3.geoGraticule().step([10, 10]);
 
-// svg.style("background-color", "blue")
+	svg.append("path")
+		.datum(graticule)
+		.attr("class", "graticule")
+		.attr("d", pathGenerator);
+	// .style("fill", "green")
+	// .style("stroke", "red");
+}
+const config = {
+	speed: 0.005,
+	verticalTilt: -15,
+	horizontalTilt: 0,
+};
+function rotation() {
+	d3.timer(function(elapsed) {
+		projection.rotate([
+			config.speed * elapsed - 120,
+			config.verticalTilt,
+			config.horizontalTilt,
+		]);
+
+		svg.selectAll("path").attr("d", pathGenerator);
+	});
+}
