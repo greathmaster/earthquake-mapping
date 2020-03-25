2cc26bd603aca5ef1db6c940f9acaabb83f4400d
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-25 10:29:06 -0700

    Added graticules to globe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import { select, json, geoPath, geoMercator, geoOrthographic, zoom, event } from "d3";
+import {
+	select,
+	json,
+	geoPath,
+	geoMercator,
+	geoOrthographic,
+	zoom,
+	event,
+	geoGraticule,
+	mouse,
+	queue
+} from "d3";
 import { feature } from "topojson";
 import "../styles/styles.css";
 
@@ -8,31 +19,54 @@ const height = +svg.attr("height");
 const projection = geoOrthographic();
 const pathGenerator = geoPath().projection(projection);
 
-svg.append("path")
+const g = svg.append("g");
+
+// var graticule = geoGraticule()
+// .step([10, 10]);
+
+g.append("path")
 	.attr("class", "sphere")
 	.attr("d", pathGenerator({ type: "Sphere" }));
 
 //TODO: consider adding debouncing, one scroll make ~100 events
 svg.call(
 	zoom().on("zoom", () => {
-		g.attr('transform', event.transform)
+		g.attr("transform", event.transform);
 	})
 );
 
 json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(
 	data => {
 		const countries = feature(data, data.objects.countries);
+		var graticule = geoGraticule().step([10, 10]);
+		// Add the graticule to the figure
+		g.append('path').datum(graticule)
+		.attr('class', 'graticule')
+		.attr('d', d => pathGenerator(d));
 
-		svg.selectAll("path")
+		g.selectAll("path.feature")
 			.data(countries.features)
 			.enter()
 			.append("path")
 			.attr("class", "country")
 			.attr("d", d => pathGenerator(d))
-			.append("title")
-			.text(d => {
-				return d.properties.name;
-			});
+			.on("mouseover", function(d) {
+				// console.log(d)
+				select(this).style("fill", "lightgrey");
+			})
+			.on("mouseout", function(d) {
+				select(this).style("fill", "white");
+			})
+			// .append("graticule")
+			// .append("path")
+			// .datum(graticule)
+			// .attr("class", "graticule")
+			// .attr("d", d => pathGenerator(d));
+
+		// .append("title")
+		// .text(d => {
+		// 	return d.properties.name;
+		// });
 	}
 );
 
