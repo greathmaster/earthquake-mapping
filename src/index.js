7c4cb6e67fd648c4384f724732498f4aaa48bd6f
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-27 12:02:09 -0700

    Basic functionality reproduced in canvas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import * as d3 from "d3";
 import { feature } from "topojson";
 import "../styles/styles.css";
+import { path } from "d3";
 
 const svg = d3.select("svg");
-const width = +svg.attr("width");
-const height = +svg.attr("height");
-const projection = d3.geoOrthographic();
-const pathGenerator = d3.geoPath().projection(projection);
+// const width = +svg.attr("width");
+// const height = +svg.attr("height");
+// const pathGenerator = d3.geoPath().projection(projection);
 
 //TODO: consider adding debouncing, one scroll make ~100 events
+const projection = d3.geoOrthographic().clipAngle(90);
 
 const config = {
 	speed: 0.005,
@@ -16,16 +17,23 @@ const config = {
 	horizontalTilt: 0,
 };
 
-svg.append("path")
-	.attr("class", "sphere")
-	.attr("d", pathGenerator({ type: "Sphere" }));
+const context = d3
+	.select("canvas")
+	.attr("width", 900)
+	.attr("height", 600)
+	.node()
+	.getContext("2d");
 
-const graticules = svg
-	.append("g")
-	.append("path")
-	.datum(d3.geoGraticule().step([10, 10]))
-	.attr("class", "graticule")
-	.attr("d", pathGenerator);
+const geoPath = d3
+	.geoPath()
+	.projection(projection)
+	.context(context);
+
+//outline for entire earth
+const sphere = { type: "Sphere" };
+
+//longitude and latitude lines
+const graticules = d3.geoGraticule()();
 
 drawEarth();
 
@@ -34,73 +42,65 @@ function drawEarth() {
 		d3.json(
 			"https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
 		),
-		d3.json(
-			"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-		),
+		// d3.json(
+		// 	"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+		// ),
 		d3.json(
 			"https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 		),
-	]).then(([geoData, earthquakeData, tectonicData]) => {
-		var countries = feature(geoData, geoData.objects.countries);
-		var country = svg
-			.append("g")
-			.selectAll("path.feature")
-			.data(countries.features)
-			.enter()
-			.append("path")
-			.attr("class", "country")
-			.attr("d", d => pathGenerator(d))
-			.on("mouseover", function(d) {
-				d3.select(this).attr("class", "country-mouse-over");
-			})
-			.on("mouseout", function(d) {
-				let c = d3.select(this);
-				c.attr("class", null);
-				c.attr("class", "country");
+		// ]).then(([geoData, earthquakeData, tectonicData]) => {
+	]).then(([geoData, tectonicData]) => {
+		var map = feature(geoData, geoData.objects.countries);
+		d3.timer(function(elapsed) {
+			context.clearRect(0, 0, 900, 600);
+			projection.rotate([
+				config.speed * elapsed,
+				config.verticalTilt,
+				config.horizontalTilt,
+			]);
+
+			context.beginPath();
+			geoPath(sphere);
+			context.fillStyle = "#D9EAEF";
+			context.fill();
+
+			map.features.forEach(d => {
+				context.beginPath();
+				geoPath(d);
+				context.fillStyle = "lightgreen";
+				context.strokeStyle = "black";
+				context.lineWidth = 0.1;
+				context.fill();
+				context.stroke();
 			});
 
-		var earthquakeLocations = svg
-			.append("g")
-			.attr("class", "points")
-			.selectAll("path")
-			.data(earthquakeData.features)
-			.enter()
-			.append("path")
-			.attr("class", "cityPoint")
-			.attr("d", pathGenerator);
+			//graticules
+			context.beginPath();
+			geoPath(graticules);
+			context.lineWidth = 0.2;
+			context.strokeStyle = "grey";
+			context.stroke();
 
-		var tectonicPlatesLines = svg
-			.append("g")
-			.selectAll("path")
-			.data(tectonicData.features)
-			.enter()
-			.append("path")
-			.attr("class", "techtonic-lines")
-			.attr("d", pathGenerator);
-
-		// const graticules = svg
-		// 	.append("g")
-		// 	.datum(d3.geoGraticule().step([10, 10]))
-		// 	.append("path")
-		// 	.attr("class", "graticule")
-		// 	.attr("d", pathGenerator);
-
-		// svg.append("g");
-
-		rotation();
-
-		function rotation() {
-			d3.timer(function(elapsed) {
-				projection.rotate([
-					config.speed * elapsed - 120,
-					config.verticalTilt,
-					config.horizontalTilt,
-				]);
-				tectonicPlatesLines.attr("d", pathGenerator);
-				earthquakeLocations.attr("d", pathGenerator);
-				country.attr("d", pathGenerator);
-				graticules.attr("d", pathGenerator);
+			tectonicData.features.forEach(d => {
+				context.beginPath();
+				geoPath(d);
+				context.fillStyle = "red";
+				context.strokeStyle = "red";
+				context.lineWidth = 0.5;
+				// context.fill();
+				context.stroke();
 			});
-		}
+
+			// earthquakeData.features.forEach(d => {
+			// 	context.beginPath();
+			// 	geoPath(d);
+			// 	context.fillStyle = "red";
+			// 	context.strokeStyle = "red";
+			// 	context.lineWidth = 5;
+			// 	context.fill();
+			// 	context.stroke();
+			// });
+		});
+		// }
 	});
 }
