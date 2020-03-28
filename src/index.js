234903ea6377c0b32505bd6a7e57d2b1979a5a79
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-27 23:52:17 -0700

    Added globe rotation stop on mouse hover

--- src/index.js
+++ src/index.js
@@ -11,18 +11,50 @@ const svg = d3.select("svg");
 //TODO: consider adding debouncing, one scroll make ~100 events
 const projection = d3.geoOrthographic().clipAngle(90);
 
-const config = {
-	speed: 0.005,
+var config = {
+	speed: 1,
 	verticalTilt: -15,
 	horizontalTilt: 0,
+	countries: null,
+	inGlobe: false,
+	timerActive: false,
+	accumulatedRotation: 0,
 };
-
-const context = d3
+const canvas = d3
 	.select("canvas")
 	.attr("width", 900)
-	.attr("height", 600)
-	.node()
-	.getContext("2d");
+	.attr("height", 600);
+
+const context = canvas.node().getContext("2d");
+
+var hiddenCanvas = d3
+	.select("canvas")
+	.attr("id", "canvas-hidden")
+	.attr("width", 900)
+	.attr("height", 600);
+
+var hiddenContext = hiddenCanvas.node().getContext("2d");
+
+var hiddenProjection = d3.geoEquirectangular();
+
+var hiddenPath = d3
+	.geoPath()
+	.projection(hiddenProjection)
+	.context(hiddenContext);
+
+//give each country a unique color
+function drawHiddenCanvas(geo) {
+	var countries = geo.features;
+	countries.forEach(function(el, i) {
+		hiddenContext.beginPath();
+		hiddenPath(el);
+		hiddenContext.fillStyle = `rgb(${i}, 0, 0)`;
+		hiddenContext.fill();
+	});
+}
+var timer;
+var currentTime;
+var prevTime = d3.now();
 
 const geoPath = d3
 	.geoPath()
@@ -49,58 +81,109 @@ function drawEarth() {
 			"https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 		),
 		// ]).then(([geoData, earthquakeData, tectonicData]) => {
-	]).then(([geoData, tectonicData]) => {
-		var map = feature(geoData, geoData.objects.countries);
-		d3.timer(function(elapsed) {
-			context.clearRect(0, 0, 900, 600);
-			projection.rotate([
-				config.speed * elapsed,
-				config.verticalTilt,
-				config.horizontalTilt,
-			]);
-
-			context.beginPath();
-			geoPath(sphere);
-			context.fillStyle = "#D9EAEF";
-			context.fill();
-
-			map.features.forEach(d => {
+		]).then(([geoData, tectonicData]) => {
+			var map = feature(geoData, geoData.objects.countries);
+			
+			timer = d3.timer(render);
+			
+			function render(elapsed) {
+				//todo use CSS to set {display: hidden} on this to hide it
+				drawHiddenCanvas(map);
+				currentTime = d3.now();
+				let diff = currentTime - prevTime;
+				prevTime = currentTime;
+				
+				// console.log([diff, elapsed])
+				if (diff < elapsed) {
+					
+				let rotation = projection.rotate()	
+				rotation[0] += diff / 50
+				rotation[1] = config.verticalTilt
+				rotation[2] = config.horizontalTilt
+				projection.rotate(rotation);
+				
+				
+				context.clearRect(0, 0, 900, 600);
+				
 				context.beginPath();
-				geoPath(d);
-				context.fillStyle = "lightgreen";
-				context.strokeStyle = "black";
-				context.lineWidth = 0.1;
+				geoPath(sphere);
+				context.fillStyle = "#D9EAEF";
 				context.fill();
-				context.stroke();
-			});
+				
+				map.features.forEach(d => {
+					context.beginPath();
+					geoPath(d);
+					context.fillStyle = "lightgreen";
+					context.strokeStyle = "black";
+					context.lineWidth = 0.1;
+					context.fill();
+					context.stroke();
+				});
 
-			//graticules
-			context.beginPath();
-			geoPath(graticules);
-			context.lineWidth = 0.2;
-			context.strokeStyle = "grey";
-			context.stroke();
-
-			tectonicData.features.forEach(d => {
+				//graticules
 				context.beginPath();
-				geoPath(d);
-				context.fillStyle = "red";
-				context.strokeStyle = "red";
-				context.lineWidth = 0.5;
-				// context.fill();
+				geoPath(graticules);
+				context.lineWidth = 0.2;
+				context.strokeStyle = "grey";
 				context.stroke();
-			});
 
-			// earthquakeData.features.forEach(d => {
-			// 	context.beginPath();
-			// 	geoPath(d);
-			// 	context.fillStyle = "red";
-			// 	context.strokeStyle = "red";
-			// 	context.lineWidth = 5;
-			// 	context.fill();
-			// 	context.stroke();
-			// });
-		});
-		// }
+				tectonicData.features.forEach(d => {
+					context.beginPath();
+					geoPath(d);
+					context.fillStyle = "red";
+					context.strokeStyle = "red";
+					context.lineWidth = 0.5;
+					// context.fill();
+					context.stroke();
+				});
+
+				// earthquakeData.features.forEach(d => {
+				// 	context.beginPath();
+				// 	geoPath(d);
+				// 	context.fillStyle = "red";
+				// 	context.strokeStyle = "red";
+				// 	context.lineWidth = 5;
+				// 	context.fill();
+				// 	context.stroke();
+				// });
+			}
+		}
+
+		function highlightPicking() {
+			const pos = d3.mouse(this);
+			const loglatArray = projection.invert(pos);
+			const hiddenPos = hiddenProjection(loglatArray);
+
+			var pickedColor = hiddenContext.getImageData(
+				hiddenPos[0],
+				hiddenPos[1],
+				1,
+				1
+			).data; // get the pixel color at mouse hover
+
+			config.inGlobe =
+				Math.abs(pos[0] - projection(projection.invert(pos))[0]) <
+					0.5 &&
+				Math.abs(pos[1] - projection(projection.invert(pos))[1]) < 0.5; // checking if the mouse is within the globe bounds
+
+			if (config.inGlobe && config.timerActive) {
+				config.timerActive = false;
+				timer.stop();
+			}
+			if (!config.inGlobe && !config.timerActive) {
+				config.timerActive = true;
+
+				timer.restart(render);
+			}
+
+			const selected =
+				config.inGlobe && pickedColor[3] === 255
+					? pickedColor[0]
+					: false; // checking for inGlobe (above) and antialiasing
+		
+					console.log(selected)
+		}
+
+		canvas.on("mousemove", highlightPicking);
 	});
 }
